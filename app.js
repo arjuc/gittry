9952eaca03486aec3d5c4bc24df51c4c476815e1
arjuc <arjunc2099@gmail.com>
2023-09-12 11:09:00 +0530

    third commit

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var header = document.getElementById("main-header");
 header.style.borderBottom = "solid 3px #000";
-var item = document.getElementsByClassName('title');
+var item = document.getElementsByClassName("title");
 item[0].style.fontWeight = "bold";
 item[0].style.backgroundColor = "yellow";
+var items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor = "green";
+
+for (var i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "bold";
+}
