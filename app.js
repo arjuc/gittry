54505430fd49a1dba2691fa1afc201f509f69630
arjuc <arjunc2099@gmail.com>
2023-09-13 09:46:40 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var header = document.getElementById("main-header");
+/*var header = document.getElementById("main-header");
 header.style.borderBottom = "solid 3px #000";
 var item = document.getElementsByClassName("title");
 item[0].style.fontWeight = "bold";
@@ -9,3 +9,18 @@ items[2].style.backgroundColor = "green";
 for (var i = 0; i < items.length; i++) {
   items[i].style.fontWeight = "bold";
 }
+*/
+
+var header = document.getElementById("main-header");
+header.style.borderBottom = "solid 3px #000";
+var li = document.getElementsByTagName("li");
+li[0].style.fontWeight = "bold";
+li[0].style.backgroundColor = "yellow";
+li[2].style.backgroundColor = "green";
+
+for (var i = 0; i < li.length; i++) {
+  li[i].style.fontWeight = "bold";
+  li[i].style.backgroundColor = "yellow";
+}
+
+
