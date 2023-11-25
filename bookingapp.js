6b57f729ddf072630db5cdeac9e5c9fcefded7c1
arjuc <arjunc2099@gmail.com>
2023-11-25 15:57:39 +0530

    delete elements

--- bookingapp.js
+++ bookingapp.js
@@ -2,6 +2,7 @@ var form = document.getElementById("addForm");
 var itemList = document.getElementById("items");
 
 form.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
 
 function addItem(e) {
   e.preventDefault();
@@ -28,13 +29,38 @@ function addItem(e) {
   var li = document.createElement("li");
   // Add class
   li.className = "list-group-item";
+  li.id = name;
+
   // Add text node with input value
   li.appendChild(document.createTextNode(name));
   li.appendChild(document.createTextNode(email));
   li.appendChild(document.createTextNode(contact));
+
+  var deleteBtn = document.createElement("button");
+
+  // Add classes to del button
+  deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode("Delete"));
+  li.appendChild(deleteBtn);
 
   // Append li to list
   itemList.appendChild(li);
 
  
 }
+
+
+// Remove item
+function removeItem(e) {
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are You Sure?")) {
+      var li = e.target.parentElement;
+      localStorage.removeItem(li.id);
+      itemList.removeChild(li);
+    }
+  }
+}
+
+
